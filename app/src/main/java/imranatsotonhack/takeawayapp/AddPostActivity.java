e07154f54944c5028f389b649840034fc5f692a9
Imran Bepari <imranbepari97@gmail.com>
2017-02-26 01:37:51 +0000

    Got database pulling working

--- app/src/main/java/imranatsotonhack/takeawayapp/AddPostActivity.java
+++ app/src/main/java/imranatsotonhack/takeawayapp/AddPostActivity.java
@@ -19,6 +19,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -30,11 +31,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -50,13 +59,15 @@ public class AddPostActivity extends AppCompatActivity implements LoaderCallback
 
     private FirebaseDatabase database;
     private DatabaseReference myRef;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("users");
 
         setContentView(R.layout.activity_add_post);
         // Set up the login form.
@@ -73,19 +84,88 @@ public class AddPostActivity extends AppCompatActivity implements LoaderCallback
             }
         });
 
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference().child("posts").child(generatePostID());
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+//                displayName = (String) dataSnapshot.child("displayName").getValue();
+//                Log.d("dbRead", "Value is: " + displayName);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w("readError", "Failed to read value.", error.toException());
+            }
+        });
+
         Button mEmailSignInButton = (Button) findViewById(R.id.submit);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(AddPostActivity.this, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show();
+
+
             }
         });
 
-        mLoginFormView = findViewById(R.id.login_form);
-        mProgressView = findViewById(R.id.login_progress);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+    private void attemptLogin() {
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            showProgress(true);
+            //attempt login
+            uploadPost();
+            Log.d("LoginMessage", "We logging in");
+
+        }
+    }
+
+    public void uploadPost() {
+
+    }
+
+    public String generatePostID() {
+        return myRef.push().getKey();
+    }
 
     /**
      * Attempts to sign in or register the account specified by the login form.
@@ -155,5 +235,40 @@ public class AddPostActivity extends AppCompatActivity implements LoaderCallback
     }
 
 
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("AddPost Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
 }
 
--- app/src/main/java/imranatsotonhack/takeawayapp/profileActivity.java
+++ app/src/main/java/imranatsotonhack/takeawayapp/profileActivity.java
@@ -4,16 +4,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -22,6 +23,8 @@ public class ProfileActivity extends AppCompatActivity {
 
     private FirebaseDatabase database;
     private DatabaseReference myRef;
+
+    private String displayName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +35,30 @@ public class ProfileActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("users/" + mPref.getString("userID", "can't find it"));
+        myRef = database.getReference().child("users").child(mPref.getString("userID", ""));
+        Log.d("ReferenceURL", myRef.toString());
 
+        final TextView displayNameField = (TextView) findViewById(R.id.displayNameField);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                displayName = (String) dataSnapshot.child("displayName").getValue();
+                displayNameField.setText(displayName);
+                Log.d("dbRead", "Value is: " + displayName);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w("readError", "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+
+        TextView email = (TextView) findViewById(R.id.emailField);
+        email.setText(mPref.getString("email", "email not found somehow"));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.addButton);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -44,13 +69,7 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        TextView displayName = (TextView) findViewById(R.id.displayName);
-        TextView email = (TextView) findViewById(R.id.emailField);
 
-//        displayName.setText(mPref.getString("name","name"));
-        email.setText(mPref.getString("email", "email not found somehow"));
-        uid = mPref.getString("userID", "null");
-//        Log.d("CHECKINGFORUSERNAME", myRef.get);
 
     }
 
